85291e943976c950556715f1a86bed473cb40945
Scott Charles <schar724@mtroyal.ca>
2022-04-01 10:36:01 -0600

    Update Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 
 public class Administrator extends Employee{
-    public Administrator(PersonalRecord personalrecods, String password) {
-        super(personalrecods, password);
-        // TODO Auto-generated constructor stub
+    public Administrator(ArrayList<DailyFitnessRecord> fitnessRecord, PersonalRecord personalrecods, String password) {
+        super(fitnessRecord, personalrecods, password);
+
     }
     public String generateEmployeeList() {
         return null;
