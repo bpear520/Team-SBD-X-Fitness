8e84df1cf24c772a8e57946dc4db7dd95e27f0ec
Scott Charles <schar724@mtroyal.ca>
2022-04-01 10:31:21 -0600

    Singleton controller

--- Controller.java
+++ Controller.java
@@ -1,16 +1,53 @@
+public final class Controller extends DataBase {
 
-public class Controller {
-    public int calculateCalorieCount() 
+    private static Controller instance;
+    public Employee currentUser = new Employee();
+
+
+    private Controller() {}
+
+    public static Controller getInstance() {
+        if(instance == null) {
+            instance = new Controller();
+        }
+        return instance;
+    }
+
+    public void initDatabase() {
+        loadFromFile();
+    }
+
+    public void createNewUser(Employee newUser) {
+        employeeList.add(newUser);
+        System.out.println(employeeList.size());
+    }
+
+    public void setCurrentUser(String userName, String password) {
+        this.currentUser = getExistingUser(userName, password);
+
+    }
+
+    public int calculateCalorieCount()
     {
-       
+
         return 0;
     }
-    public void sortData() 
+    public void sortData()
     {
-        
+
     }
     public void notifyAchievement()
     {
-        
+
     }
+
+    public void setCurrentUser(Employee curUser) {
+        this.currentUser = curUser;
+    }
+
+    public Employee getCurrentUser() {
+        return this.currentUser;
+    }
+
 }
+
