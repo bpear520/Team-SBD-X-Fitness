0043a6713c28b1b4cad9009cf4c32bef27a61c5a
Scott Charles <schar724@mtroyal.ca>
2022-04-04 21:12:20 -0600

    Update Achievement.java

--- Achievement.java
+++ Achievement.java
@@ -1,162 +1,84 @@
-import java.util.ArrayList;
+
 
 public class Achievement {
 
-    private boolean tenDailyWorkout = false;
-    private boolean twentyDailyWorkout = false;
-    private boolean threePerfectDays = false;
-    private boolean sixPerfectDays = false;
-    private boolean twentyPerfectDays = false;
-    private boolean goalWeight = false;
-    private int recCalories;
-    private Employee emp;
+    private boolean tenDailyWorkout;
+    private boolean twentyDailyWorkout;
+    private boolean threePerfectDays;
+    private boolean sixPerfectDays;
+    private boolean twentyPerfectDays;
+    private boolean goalWeightReached;
 
-    Controller controller = Controller.getInstance();
-    private ArrayList<DailyFitnessRecord> curEmpFitRec = new ArrayList<DailyFitnessRecord>();
-    private int numRecords;
-
-    public Achievement(Employee emp) {
-        this.emp = emp;
-        curEmpFitRec = emp.getFitnessRecords();
-        numRecords = curEmpFitRec.size();
-        updateAchievements();
-    }
-
-    public void updateAchievements() {
-        setWorkoutAchievements();
-        setPerfectDayAchievements();
-        setGoalWeightAcheivement();
-    }
-
-    public void setWorkoutAchievements() {
-        int dailyWorkoutCount = 0;
-
-        for(int i = 0; i < numRecords - 1; i++) {
-            if(curEmpFitRec.get(i).isWorkedOut()) {
-                dailyWorkoutCount++;
-            }
-        }
-
-        if(dailyWorkoutCount >= 10 && dailyWorkoutCount < 20) {
-            setTenDailyWorkout();
-        } else if (dailyWorkoutCount >= 20) {
-            setTwentyDailyWorkout();
-        }
-    }
-
-    public void setPerfectDayAchievements() {
-        boolean caloriesMet = false;
-        boolean workedOut = false;
-        boolean greatSleep = false;
-        int perfectDayCount = 0;
-
-        getRecCalories();
-        perfectDayCount = getPerfectDayCount(caloriesMet, workedOut, greatSleep, perfectDayCount);
-        setPerfectDays(perfectDayCount);
-    }
-
-    /**
-     * @param perfectDayCount
-     */
-    private void setPerfectDays(int perfectDayCount) {
-        if(perfectDayCount >= 3 && perfectDayCount < 6) {
-            setThreePerfectDays();
-        } else if (perfectDayCount >= 6 && perfectDayCount < 20) {
-            setSixPerfectDays();
-        } else if (perfectDayCount >= 20) {
-            setTwentyPerfectDays();
-        }
-    }
-
-    /**
-     *
-     */
-    private void getRecCalories() {
-        if(emp.getPersonalrecods().getGender() == "Male") {
-            recCalories = 2500;
-        } else {
-            recCalories = 1900;
-        }
-    }
-
-    /**
-     * @param caloriesMet
-     * @param workedOut
-     * @param greatSleep
-     * @param perfectDayCount
-     * @return
-     */
-    private int getPerfectDayCount(boolean caloriesMet, boolean workedOut, boolean greatSleep, int perfectDayCount) {
-        for(int i = 0; i < numRecords - 1; i++) {
-            if(curEmpFitRec.get(i).getCalories() <= recCalories) {
-                caloriesMet = true;
-            }
-
-            if(curEmpFitRec.get(i).isWorkedOut()) {
-                workedOut = true;
-            }
-
-            if(curEmpFitRec.get(i).getSleepQuality().equals("Great")) {
-                greatSleep = true;
-            }
-
-            if(caloriesMet && workedOut && greatSleep) {
-                perfectDayCount++;
-            }
-        }
-        return perfectDayCount;
-    }
-
-    private void setGoalWeightAcheivement() {
-        if(emp.getPersonalrecods().getWeight() == emp.getPersonalrecods().getGoalWeight()) {
-            goalWeight = true;
-        }
-    }
-
-    public void setThreePerfectDays() {
-        this.threePerfectDays = true;
-    }
-
-    public void setSixPerfectDays() {
-        this.sixPerfectDays = true;
-    }
-
-    public void setTwentyPerfectDays() {
-        this.twentyPerfectDays = true;
-    }
-
-    public void setTenDailyWorkout(){
-        this.tenDailyWorkout = true;
-    }
-
-    public void setTwentyDailyWorkout(){
-        this.twentyDailyWorkout = true;
+    public Achievement(boolean tenDailyWorkout, boolean twentyDailyWorkout, boolean threePerfectDays, boolean sixPerfectDays, boolean twentyPerfectDays, boolean goalWeightReached) {
+       this.tenDailyWorkout = tenDailyWorkout;
+       this.twentyDailyWorkout = twentyDailyWorkout;
+       this.threePerfectDays = threePerfectDays;
+       this.sixPerfectDays = sixPerfectDays;
+       this.twentyPerfectDays = twentyPerfectDays;
+       this.goalWeightReached = goalWeightReached;
     }
 
     public boolean isTenDailyWorkout() {
         return tenDailyWorkout;
     }
 
+    public void setTenDailyWorkout(boolean tenDailyWorkout) {
+        this.tenDailyWorkout = tenDailyWorkout;
+    }
+
     public boolean isTwentyDailyWorkout() {
         return twentyDailyWorkout;
+    }
+
+    public void setTwentyDailyWorkout(boolean twentyDailyWorkout) {
+        this.twentyDailyWorkout = twentyDailyWorkout;
     }
 
     public boolean isThreePerfectDays() {
         return threePerfectDays;
     }
 
+    public void setThreePerfectDays(boolean threePerfectDays) {
+        this.threePerfectDays = threePerfectDays;
+    }
+
     public boolean isSixPerfectDays() {
         return sixPerfectDays;
+    }
+
+    public void setSixPerfectDays(boolean sixPerfectDays) {
+        this.sixPerfectDays = sixPerfectDays;
     }
 
     public boolean isTwentyPerfectDays() {
         return twentyPerfectDays;
     }
 
-    public boolean isGoalWeight() {
-        return goalWeight;
+    public void setTwentyPerfectDays(boolean twentyPerfectDays) {
+        this.twentyPerfectDays = twentyPerfectDays;
     }
-    public void setGoalWeight(boolean goalWeight) {
-        this.goalWeight = goalWeight;
+
+    public boolean isGoalWeightReached() {
+        return goalWeightReached;
     }
+
+    public void setGoalWeightReached(boolean goalWeightReached) {
+        this.goalWeightReached = goalWeightReached;
+    }
+
+    public String formatToFile() {
+        String toFile = String.valueOf(tenDailyWorkout) + ";" +
+                        String.valueOf(twentyDailyWorkout) + ";" +
+                        String.valueOf(threePerfectDays) + ";" +
+                        String.valueOf(sixPerfectDays) + ";" +
+                        String.valueOf(twentyPerfectDays) + ";" +
+                        String.valueOf(goalWeightReached) + ";";
+        return toFile;
+    }
+
+
+
+
+
+
 }
